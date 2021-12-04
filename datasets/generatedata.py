5e36c5c955dc57dc0d8ef1f765a407821699c351
allen.rrivas30@csu.fullerton.edu <allen.rrivas30@csu.fullerton.edu>
2021-12-04 12:34:35 -0800

    Update generatedata.py which creates a test example csv file which holds song, artitst, genre, subgenre, and q1-q5.

---
+++ datasets/generatedata.py
@@ -0,0 +1,19 @@
+import csv
+
+# Create a list of column names.
+columns = ['song', 'artist', 'genre', 'subgenre', 'q1', 'q2', 'q3', 'q4', 'q5']
+
+# Create a list of rows that match the column's data.
+rows = [
+    ['Martini Blue', 'DPR Live', 'Rap', 'K-Pop',
+        'a', 'a', 'b', 'c', 'a']
+]
+
+# Create a filename.
+filename = "testMuzyka.csv"
+
+# Open filename that writes in the columns and rows variables.
+with open(filename, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(columns)
+    writer.writerows(rows)
