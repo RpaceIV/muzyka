1f1e6ed9dc2cfd04468cf2d7603cb6ba98095731
Robert Pace <rpaceiv@gmail.com>
2021-12-06 04:06:35 -0800

    Modified testspotify.py. This script can now loop through a chosen amount of songs and grabs/displays the metadata on those songs.

--- datasets/testspotify.py
+++ datasets/testspotify.py
@@ -12,31 +12,31 @@ import time
 import sys
 
 # SubGenre = namedtuple('SubGenre', 'name,')
-
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 def main():
     '''Search for a playlist'''
-    if len(sys.argv) > 1:
-        search_str = sys.argv[1]
-    else:
+    # if len(sys.argv) > 1:
+    #     search_str = sys.argv[1]
+    # else:
         # search_str = 'track:Lionhearted artist:Porter Robinson'
         # type_str = 'track,artist'
-        search_str = 'playlist:The Sound of breakcore AND The Sounds of Spotify'
-        type_str = 'playlist'
-        market_str = 'US'
-        limit_int = 1
+    search_str = 'playlist:The Sound of breakcore AND The Sounds of Spotify'
+    type_str = 'playlist'
+    market_str = 'US'
+    limit_int = 1
+    offs = 0
 
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-    result = sp.search(search_str,limit_int,0,type_str,market_str) #this returns a type dict and is stored in result
+    result = sp.search(search_str,limit_int,offs,type_str,market_str) #this returns a type dict and is stored in result
     # pprint.pprint(result['playlists']['items'][0]['id'])
     
     '''Get tracks from a playlist'''
-    pl_id = 'spotify:playlist:'+result['playlists']['items'][0]['id'] #'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
+    pl_id = 'spotify:playlist:'+result['playlists']['items'][0]['id']
     # pl_id = 'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
     # print()
     offset = 0
 
-    # for x in range(6):
+    # for s in range(6):
     response = sp.playlist_items(pl_id,
                                 limit=6,
                                 offset=offset,
@@ -46,31 +46,30 @@ def main():
     #     break
     
     # pprint(response['items'])
-    # for x in range(6):
+    for s in range(6):
     #     pprint(response['items'][x]['track']['id'])
-    
-    pprint(response['items'][0]['track']['id'])
-    # offset = offset + len(response['items'])
-    # print(offset, "/", response['total'])
+        pprint(response['items'][s]['track']['id'])
+        # offset = offset + len(response['items'])
+        # print(offset, "/", response['total'])
 
-    '''Track Name'''
-    trackk = sp.track(response['items'][0]['track']['id'])
-    print(trackk['name'])
-    print(trackk['album']['artists'][0]['name'])
-    print(trackk['album']['name'])
-    
+        '''Track Name'''
+        trackk = sp.track(response['items'][s]['track']['id'])
+        print(trackk['name'])
+        print(trackk['album']['artists'][0]['name'])
+        print(trackk['album']['name'])
+        
 
-    '''Audio Features'''
-    sp.trace = True
-    # if len(sys.argv) > 1:
-    # tids = sys.argv[1:]
-    # print(tids)
+        '''Audio Features'''
+        sp.trace = True
+        # if len(sys.argv) > 1:
+        # tids = sys.argv[1:]
+        # print(tids)
 
-    start = time.time()
-    features = sp.audio_features(response['items'][0]['track']['id'])
-    delta = time.time() - start
-    print(json.dumps(features, indent=4))
-    print("features retrieved in %.2f seconds" % (delta,))
+        start = time.time()
+        features = sp.audio_features(response['items'][s]['track']['id'])
+        delta = time.time() - start
+        print(json.dumps(features, indent=4))
+        print("features retrieved in %.2f seconds" % (delta,))
 
 if __name__ == "__main__":
     main()
