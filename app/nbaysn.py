c883ea96162a8f589f5922628f6b8805b5efebd9
Robert Pace <rpaceiv@gmail.com>
2021-12-08 20:22:38 -0800

    Worked on the nbaysn.py file. It can now print a tuple of summarized dataset for each column, which would be (mean,std_deviation,length of column).

--- app/nbaysn.py
+++ app/nbaysn.py
@@ -5,42 +5,78 @@ from math import exp
 from math import pi
 import pandas as pd
 
+# Split the dataset by class values, returns a dictionary
 def separate_by_class(dataset):
     separated = dict()
     for i in range(len(dataset)):
         vector = dataset[i]
-        class_value = vector[-12]
-        if(class_value not in separated):
+        class_value = vector[-1]
+        if (class_value not in separated):
             separated[class_value] = list()
         separated[class_value].append(vector)
     return separated
-
-# def mean(numbers):
-#     return sum(numbers)/float(len(numbers))
-
-# def stdev(numbers):
-
+ 
+# Calculate the mean of a list of numbers
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+ 
+# Calculate the standard deviation of a list of numbers
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
+    return sqrt(variance)
+ 
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+    summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
+    del(summaries[-1])
+    return summaries
+ 
+# Split dataset by class then calculate statistics for each row
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
 
 # def main():
 
 
 if __name__ == "__main__":
-    # dfTwo.to_csv("test2.csv", index=False, mode='a', header=False)
+    # dfTwo.to_csv("test2.csv", index=False, mode=1, header=False)
     # dataset = pd.read_csv('../datasets/test.csv')
     # print(dataset['song'])
-    dataset = [['The Sound of Deep House','Finally','Kings Of Tomorrow','It\'s In the Lifestyle (Limited Edition)','electronic','deep house',0.010278456,'b','b','a','b','z',59,0.859,0.584,-10.933,0.833],
-                ['The Sound of Swancore','Lemonade,Dwellings,Lavender Town','rock','swancore',0.007299506999999999,'b','b','c','b','z',43,0.338,0.924,-4.204,0.18],
-                ['The Sound of Swancore','Deleto,Wolf & Bear,Deleto','rock','swancore',0.007299506999999999,'b','b','c','b','z',43,0.541,0.986,-4.434,0.635],
-                ['The Sound of Hard Bop','Alone Together - Rudy Van Gelder Remaster,Kenny Dorham,Quiet Kenny','jazz','hard bop',0.008725868000000001,'b','b','d','a','z',56,0.336,0.0619,-20.494,0.197],
-                ['The Sound of Hard Bop','Blues Inn - Remastered,Jackie McLean,Jackie\'s Bag','jazz','hard bop',0.008725868000000001,'b','b','d','b','z',49,0.583,0.461,-9.58,0.516],
-                ['The Sound of Hard Bop','True Blue,Tina Brooks,True Blue','jazz','hard bop',0.008725868000000001,'b','b','d','a','z',41,0.631,0.514,-6.68,0.768]]
+    # dataset = [['The Sound of Deep House','Finally','Kings Of Tomorrow','It\'s In the Lifestyle (Limited Edition)','electronic','deep house',0.010278456,2,2,1,2,5,59,0.859,0.584,-10.933,0.833],
+    #             ['The Sound of Swancore','Lemonade,Dwellings,Lavender Town','rock','swancore',0.007299506999999999,2,2,3,2,5,43,0.338,0.924,-4.204,0.18],
+    #             ['The Sound of Swancore','Deleto,Wolf & Bear,Deleto','rock','swancore',0.007299506999999999,2,2,3,2,5,43,0.541,0.986,-4.434,0.635],
+    #             ['The Sound of Hard Bop','Alone Together - Rudy Van Gelder Remaster,Kenny Dorham,Quiet Kenny','jazz','hard bop',0.008725868000000001,2,2,4,1,5,56,0.336,0.0619,-20.494,0.197],
+    #             ['The Sound of Hard Bop','Blues Inn - Remastered,Jackie McLean,Jackie\'s Bag','jazz','hard bop',0.008725868000000001,2,2,4,2,5,49,0.583,0.461,-9.58,0.516],
+    #             ['The Sound of Hard Bop','True Blue,Tina Brooks,True Blue','jazz','hard bop',0.008725868000000001,2,2,4,1,5,41,0.631,0.514,-6.68,0.768]]
+    
 
-    separated = separate_by_class(dataset)
+    dataset = [[2,2,1,2,3,0],#'deep house'],
+                [1,2,3,2,4,1],#'swancore'],
+                [2,1,3,2,5,2],#'swancore'],
+                [2,2,4,1,2,3],#'hard bop'],
+                [1,2,4,4,5,4],#'hard bop'],
+                [2,2,4,1,1,5]]#'hard bop']]
+    
+    summary = summarize_dataset(dataset)
+    print(summary)
+
+    # separated = separate_by_class(dataset)
     # print(separated)
-    for label in separated:
-        print(label)
-        for row in separated[label]:
-            print(row)
+    # for label in separated:
+    #     print(label)
+    #     for row in separated[label]:
+    #         print(row)
+    # summary = summarize_by_class(dataset)
+    # for label in summary:
+    #     print(label)
+    #     for row in summary[label]:
+    #         print(row)
+
     # print(df.head())
 
 
